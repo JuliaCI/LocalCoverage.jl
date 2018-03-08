1facfe4b524f754d1527df4b89ab70c804cfb4e4
Tamas K. Papp <tkpapp@gmail.com>
2018-03-08 15:43:42 +0100

    Removed tests from block (easier debugging).

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
 using LocalCoverage
 using Base.Test
 
-@testset "coverage generation and cleanup" begin
-    @test !isfile("coverage/lcov.info")
-    generate_coverage("LocalCoverage"; genhtml = false)
-    @test isfile("coverage/lcov.info")
-    clean_coverage("LocalCoverage")
-    @test !isdir("coverage")
-end
+@test !isfile("coverage/lcov.info")
+generate_coverage("LocalCoverage"; genhtml = false)
+@test isfile("coverage/lcov.info")
+clean_coverage("LocalCoverage")
+@test !isdir("coverage")
