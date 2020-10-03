b1c69a9aba1df1a57b8a696eb0b6bcb1b3679035
ExpandingMan <ExpandingMan@users.noreply.github.com>
2020-10-03 04:57:24 -0400

    cleanup and modernization (#13)
    * cleanup and modernization
    
    * bumped version number
    
    * added build.log to .gitignore
    
    * updated travis script
    
    * tweaked readme and added some token tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,15 @@ lockfile = "/tmp/testingLocalCoverage" # prevent infinite recursion when testing
 covdir = normpath(joinpath(@__DIR__, "..", "coverage"))
 
 if !isfile(lockfile)
+    @test isdir(LocalCoverage.pkgdir("LocalCoverage"))
     tracefile = joinpath(covdir, "lcov.info")
     @test !isfile(tracefile)
     touch(lockfile)
-    generate_coverage("LocalCoverage"; genhtml = false)
+    cov = generate_coverage("LocalCoverage"; genhtml=true, show_summary=false)
+    str = LocalCoverage.coverage_summary_string(cov)
+    @test occursin("Coverage", str)
     rm(lockfile)
     @test isfile(tracefile)
-    clean_coverage("LocalCoverage")
+    clean_coverage("LocalCoverage", rm_directory=true)
     @test !isdir(covdir)
 end
