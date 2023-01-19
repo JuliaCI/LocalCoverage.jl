d35b8e6b699484f69a0097ee5b5fcdae47fb081f
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2023-01-19 13:25:09 +0100

    WIP - Naive implementation of LCOV -> Cobertura conversion - Not tested inside the package (tests fail for some reason ?) - Comments are welcome

--- src/LocalCoverage.jl
+++ src/LocalCoverage.jl
@@ -7,6 +7,9 @@ import DefaultApplication
 import LibGit2
 using UnPack: @unpack
 import Pkg
+import Dates
+using EzXML
+using OrderedCollections
 
 export generate_coverage, clean_coverage, report_coverage_and_exit, html_coverage, generate_xml
 
@@ -85,12 +88,12 @@ format_gaps(summary::FileCoverageSummary) = join(map(format_gap, summary.coverag
 function format_line(summary::Union{PackageCoverage,FileCoverageSummary})
     @unpack lines_hit, lines_tracked, coverage_percentage = summary
     is_pkg = summary isa PackageCoverage
-    (name = is_pkg ? "TOTAL" : summary.filename,
-     total = lines_tracked,
-     hit = lines_hit,
-     missed = lines_tracked - lines_hit,
-     coverage_percentage,
-     gaps = is_pkg ? "" : format_gaps(summary))
+    (name=is_pkg ? "TOTAL" : summary.filename,
+        total=lines_tracked,
+        hit=lines_hit,
+        missed=lines_tracked - lines_hit,
+        coverage_percentage,
+        gaps=is_pkg ? "" : format_gaps(summary))
 end
 
 function Base.show(io::IO, summary::PackageCoverage)
@@ -118,25 +121,25 @@ function Base.show(io::IO, summary::PackageCoverage)
     highlighters = (
         Highlighter(
             (data, i, j) -> j == percentage_column && row_coverage[i] <= 50,
-            bold = true,
-            foreground = :red,
+            bold=true,
+            foreground=:red,
         ),
-        Highlighter((data, i, j) -> j == percentage_column && row_coverage[i] <= 70, foreground = :yellow),
-        Highlighter((data, i, j) -> j == percentage_column && row_coverage[i] >= 90, foreground = :green),
+        Highlighter((data, i, j) -> j == percentage_column && row_coverage[i] <= 70, foreground=:yellow),
+        Highlighter((data, i, j) -> j == percentage_column && row_coverage[i] >= 90, foreground=:green),
     )
 
     pretty_table(
         io,
         rows;
-        title = "Coverage of $(package_dir)",
+        title="Coverage of $(package_dir)",
         header,
         alignment,
-        crop = :none,
-        linebreaks = true,
+        crop=:none,
+        linebreaks=true,
         columns_width,
-        autowrap = true,
+        autowrap=true,
         highlighters,
-        body_hlines = [length(rows) - 1],
+        body_hlines=[length(rows) - 1]
     )
 end
 
@@ -177,12 +180,12 @@ function eval_coverage_metrics(coverage, package_dir)
         @unpack coverage = file
         lines_tracked = count(!isnothing, coverage)
         coverage_gaps = find_gaps(coverage)
-        lines_hit = lines_tracked - sum(length, coverage_gaps; init = 0)
+        lines_hit = lines_tracked - sum(length, coverage_gaps; init=0)
         filename = relpath(file.filename, package_dir)
         FileCoverageSummary(; filename, lines_hit, lines_tracked, coverage_gaps)
     end
-    lines_hit = sum(x -> x.lines_hit, files; init = 0)
-    lines_tracked = sum(x -> x.lines_tracked, files; init = 0)
+    lines_hit = sum(x -> x.lines_hit, files; init=0)
+    lines_tracked = sum(x -> x.lines_tracked, files; init=0)
     PackageCoverage(; package_dir, files, lines_hit, lines_tracked)
 end
 
@@ -210,9 +213,9 @@ show(IOContext(stdout, :print_gaps => true), cov) # print coverage gap informati
 ```
 
 """
-function generate_coverage(pkg = nothing; run_test = true)
+function generate_coverage(pkg=nothing; run_test=true)
     if run_test
-        isnothing(pkg) ? Pkg.test(; coverage = true) : Pkg.test(pkg; coverage = true)
+        isnothing(pkg) ? Pkg.test(; coverage=true) : Pkg.test(pkg; coverage=true)
     end
     package_dir = pkgdir(pkg)
     cd(package_dir) do
@@ -233,10 +236,10 @@ Clean up after [`generate_coverage`](@ref).
 If `rm_directory`, will delete the coverage directory, otherwise only deletes the
 `$(LCOVINFO) file.
 """
-function clean_coverage(pkg = nothing; rm_directory::Bool = true)
+function clean_coverage(pkg=nothing; rm_directory::Bool=true)
     cd(pkgdir(pkg)) do
         if rm_directory
-            rm(COVDIR; force = true, recursive = true)
+            rm(COVDIR; force=true, recursive=true)
         else
             rm(joinpath(COVDIR, LCOVINFO))
         end
@@ -251,7 +254,7 @@ inside `dir`.
 
 See [`generate_coverage`](@ref).
 """
-function html_coverage(coverage::PackageCoverage; open = false, dir = tempdir())
+function html_coverage(coverage::PackageCoverage; open=false, dir=tempdir())
     cd(coverage.package_dir) do
         branch = try
             LibGit2.headname(GitRepo("./"))
@@ -277,8 +280,8 @@ function html_coverage(coverage::PackageCoverage; open = false, dir = tempdir())
     nothing
 end
 
-function html_coverage(pkg = nothing; open = false, dir = tempdir())
-    html_coverage(generate_coverage(pkg), open = open, dir = dir)
+function html_coverage(pkg=nothing; open=false, dir=tempdir())
+    html_coverage(generate_coverage(pkg), open=open, dir=dir)
 end
 
 """
@@ -306,19 +309,19 @@ be generated.
 - `io` can be used for redirecting the output
 """
 function report_coverage_and_exit(coverage::PackageCoverage;
-                                  target_coverage::Real = 80,
-                                  print_summary::Bool = true,
-                                  print_gaps::Bool = false,
-                                  io::IO = stdout)
+    target_coverage::Real=80,
+    print_summary::Bool=true,
+    print_gaps::Bool=false,
+    io::IO=stdout)
     was_target_met = coverage.coverage_percentage >= target_coverage
     print_summary && show(IOContext(io, :print_gaps => print_gaps), coverage)
     print(io, " Target coverage ", was_target_met ? "was met" : "wasn't met", " (")
-    printstyled(io, "$target_coverage%", color = was_target_met ? :green : :red, bold = true)
+    printstyled(io, "$target_coverage%", color=was_target_met ? :green : :red, bold=true)
     println(io, ")")
     exit(was_target_met ? 0 : 1)
 end
 
-function report_coverage_and_exit(pkg = nothing; kwargs...)
+function report_coverage_and_exit(pkg=nothing; kwargs...)
     coverage = generate_coverage(pkg)
     report_coverage_and_exit(coverage; kwargs...)
 end
@@ -335,8 +338,309 @@ This requires the Python package `lcov_cobertura` (>= v2.0.1), available in PyPl
 """
 function generate_xml(coverage::PackageCoverage, filename="cov.xml")
     run(Cmd(Cmd(["lcov_cobertura", "lcov.info", "-o", filename]),
-            dir = joinpath(coverage.package_dir, COVDIR)))
+        dir=joinpath(coverage.package_dir, COVDIR)))
     @info("generated cobertura XML $filename")
 end
+
+"""
+$(SIGNATURES)
+
+Convert a LCOV coverage file to a Cobertura coverage file. Relies on EzXML
+"""
+function writexml(xmlpath::String, lcovpath::String)
+    lcov = LcovCobertura(lcovpath)
+    xmlcoverage = convert(lcov)
+    open(xmlpath, "w") do io
+        prettyprint(io, xmlcoverage)
+    end
+end
+
+"""
+Property container for LCOV -> Cobertura conversion
+
+$(FIELDS)
+"""
+# TODO see if this is really necessary...
+struct LcovCobertura
+    lcov_data::String
+    base_dir::String
+    excludes::Function
+    format::Function
+    function LcovCobertura(lcov_data, base_dir=".", excludes=x -> false, demangle=false::Bool)
+        if demangle
+            demangler = Demangler()
+            format = demangler.demangle
+        else
+            format = identity
+        end
+        return new(lcov_data, base_dir, excludes, format)
+    end
+end
+
+"""
+$(SIGNATURES)
+
+Convert a LCOV file to a nested dictionary structure ready fr XML conversion
+"""
+function parse(lcov::LcovCobertura; timestamp=round(Int, datetime2unix(Dates.now())))
+    coverage_data = OrderedDict(
+        "packages" => OrderedDict{String,Any}(),
+        "summary" => OrderedDict(
+            "lines-total" => 0,
+            "lines-covered" => 0,
+            "branches-total" => 0,
+            "branches-covered" => 0
+        ),
+        "timestamp" => timestamp
+    )
+    package = nothing
+    current_file = nothing
+    file_lines_total = 0
+    file_lines_covered = 0
+    file_lines = OrderedDict()
+    file_methods = OrderedDict()
+    file_branches_total = 0
+    file_branches_covered = 0
+
+    for line in eachline(lcov.lcov_data)
+        if strip(line) == "end_of_record"
+            if !isnothing(current_file)
+                package_dict = coverage_data["packages"][package]
+                package_dict["lines-total"] += file_lines_total
+                package_dict["lines-covered"] += file_lines_covered
+                package_dict["branches-total"] += file_branches_total
+                package_dict["branches-covered"] += file_branches_covered
+                file_dict = package_dict["classes"][current_file]
+                file_dict["lines-total"] = file_lines_total
+                file_dict["lines-covered"] = file_lines_covered
+                file_dict["lines"] = deepcopy(file_lines)
+                file_dict["methods"] = deepcopy(file_methods)
+                file_dict["branches-total"] = file_branches_total
+                file_dict["branches-covered"] = file_branches_covered
+                coverage_data["summary"]["lines-total"] += file_lines_total
+                coverage_data["summary"]["lines-covered"] += file_lines_covered
+                coverage_data["summary"]["branches-total"] += file_branches_total
+                coverage_data["summary"]["branches-covered"] += file_branches_covered
+            end
+        end
+        line_parts = split(line, ":", limit=2)
+        input_type = first(line_parts)
+        if input_type == "SF"
+            # Get file name
+            file_name = strip(last(line_parts))
+            relative_file_name = relpath(file_name, lcov.base_dir)
+            package = join(splitpath(relative_file_name)[1:end-1], '.')
+            class_name = join(splitpath(relative_file_name), '.')
+            if !(package in keys(coverage_data["packages"]))
+                coverage_data["packages"][package] = OrderedDict(
+                    "classes" => OrderedDict(),
+                    "lines-total" => 0,
+                    "lines-covered" => 0,
+                    "branches-total" => 0,
+                    "branches-covered" => 0
+                )
+            end
+            coverage_data["packages"][package]["classes"][relative_file_name] = OrderedDict(
+                "name" => class_name,
+                "lines" => OrderedDict(),
+                "lines-total" => 0,
+                "lines-covered" => 0,
+                "branches-total" => 0,
+                "branches-covered" => 0
+            )
+            package = package
+            current_file = relative_file_name
+            file_lines_total = 0
+            file_lines_covered = 0
+            empty!(file_lines)
+            empty!(file_methods)
+            file_branches_total = 0
+            file_branches_covered = 0
+        elseif input_type == "DA"
+            # DA:2,0
+            (line_number, line_hits) = split(strip(last(line_parts)), ",")[1:2]
+            if !(line_number in keys(file_lines))
+                file_lines[line_number] = OrderedDict(
+                    "branch" => "false",
+                    "branches-total" => 0,
+                    "branches-covered" => 0
+                )
+                file_lines[line_number]["hits"] = line_hits
+                # Increment lines total/covered for class and package
+                if tryparse(Int64, line_hits) > 0
+                    file_lines_covered += 1
+                end
+                file_lines_total += 1
+            elseif input_type == "BRDA"
+                # BRDA:1,1,2,0
+                (line_number, block_number, branch_number, branch_hits) = split(strip(last(line_parts)), ",")
+                if !(line_number in keys(file_lines))
+                    file_lines[line_number] = OrderedDict(
+                        "branch" => "true",
+                        "branches-total" => 0,
+                        "branches-covered" => 0,
+                        "hits" => 0
+                    )
+                end
+                file_lines[line_number]["branch"] = "true"
+                file_lines[line_number]["branches-total"] += 1
+                file_branches_total += 1
+                if branch_hits != "-" && branch_hits > 0
+                    file_lines[line_number]["branches-covered"] += 1
+                    file_branches_covered += 1
+                end
+            elseif input_type == "BRF"
+                file_branches_total = first(line_parts)
+            elseif input_type == "BRH"
+                file_branches_covered = first(line_parts)
+            elseif input_type == "FN"
+                # FN:5,(anonymous_1)
+                function_line, function_name = split(strip(last(line_parts)); limit=2)
+                file_methods[function_name] = [function_line, "0"]
+            elseif input_type == "FNDA"
+                # FNDA:0,(anonymous_1)
+                (function_hits, function_name) = split(strip(last(line_parts)); limit=2)
+                if !(function_name in file_methods)
+                    file_methods[function_name] = ["0", "0"]
+                    file_methods[function_name][end] = function_hits
+                end
+            end
+        end
+
+        # TODO figure a julian way to exclude some packages, e.g. with a function as in `filter`
+        # Exclude packages
+        # excluded = [x for x in keys(coverage_data["packages"]) for exc in lcov.excludes if exc == x]
+        # for package in excluded
+        #     delete!(coverage_data["packages"], package)
+        # end
+
+        # Compute line coverage rates
+        for package_data in values(coverage_data["packages"])
+            package_data["line-rate"] = _percent(package_data["lines-total"], package_data["lines-covered"])
+            package_data["branch-rate"] = _percent(package_data["branches-total"], package_data["branches-covered"])
+        end
+    end
+    return coverage_data
+end
+
+
+"""
+$(SIGNATURES)
+
+Convert lcov file to cobertura XML using options from this instance.
+"""
+function convert(lcov::LcovCobertura)
+    coverage_data = parse(lcov)
+    return generate_cobertura_xml(lcov, coverage_data)
+end
+
+
+"""
+$(SIGNATURES)
+
+Given parsed coverage data, return a String cobertura XML representation.
+"""
+function generate_cobertura_xml(lcov::LcovCobertura, coverage_data)
+    # doctype = EzXML.readdtd("http://cobertura.sourceforge.net/xml/coverage-04.dtd")
+    document = EzXML.XMLDocument()
+    root = ElementNode("coverage")
+    setroot!(document, root)
+    summary = coverage_data["summary"]
+    root["branch-rate"] = _percent(summary["branches-total"], summary["branches-covered"])
+    root["branches-covered"] = string(summary["branches-covered"])
+    root["branches-valid"] = string(summary["branches-total"])
+    root["complexity"] = "0"
+    root["line-rate"] = _percent(summary["lines-total"], summary["lines-covered"])
+    root["lines-covered"] = string(summary["lines-covered"])
+    root["lines-valid"] = string(summary["lines-total"])
+    root["timestamp"] = coverage_data["timestamp"]
+    root["version"] = "2.0.3"
+
+
+    sources = ElementNode("sources")
+    link!(root, sources)
+
+    source = ElementNode("source")
+    link!(sources, source)
+    link!(source, TextNode(lcov.base_dir))
+
+
+    packages_el = ElementNode("packages")
+
+    packages = coverage_data["packages"]
+    for (package_name, package_data) in packages
+        package_el = ElementNode("package")
+        package_el["line-rate"] = package_data["line-rate"]
+        package_el["branch-rate"] = package_data["branch-rate"]
+        package_el["name"] = package_name
+        package_el["complexity"] = "0"
+
+        classes_el = ElementNode("classes")
+        for (class_name, class_data) in package_data["classes"]
+            class_el = ElementNode("class")
+            class_el["branch-rate"] = _percent(class_data["branches-total"], class_data["branches-covered"])
+            class_el["complexity"] = "0"
+            class_el["filename"] = class_name
+            class_el["line-rate"] = _percent(class_data["lines-total"], class_data["lines-covered"])
+            class_el["name"] = class_data["name"]
+            # link!(classes_el, class_el)
+            # Process methods
+            methods_el = ElementNode("methods")
+            for (method_name, (line, hits)) in class_data["methods"]
+                method_el = ElementNode("method")
+                method_el["name"] = format(method_name)
+                method_el["signature"] = ""
+                method_el["line-rate"] = hits > 0 ? "1.0" : "0.0"
+                method_el["branch-rate"] = hits > 0 ? "1.0" : "0.0"
+
+                method_lines_el = ElementNode("lines")
+                method_line_el = ElementNode("line")
+                method_line_el["hits"] = hits
+                method_line_el["number"] = line
+                method_line_el["branch"] = "false"
+
+                link!(method_lines_el, method_line_el)
+                link!(method_el, method_lines_el)
+                link!(methods_el, method_el)
+            end
+            # Process lines
+            lines_el = ElementNode("lines")
+            lines = keys(class_data["lines"])
+            for line_number in lines
+                line_el = ElementNode("line")
+                line_el["branch"] = class_data["lines"][line_number]["branch"]
+                line_el["hits"] = string(class_data["lines"][line_number]["hits"])
+                line_el["number"] = string(line_number)
+
+                if class_data["lines"][line_number]["branch"] == "true"
+                    total = class_data["lines"][line_number]["branches-total"]
+                    covered = class_data["lines"][line_number]["branches-covered"]
+                    percentage = (covered * 100.0) / total
+                    line_el["condition-coverage"] = "$percentage % ($covered/$total)"
+                end
+                link!(lines_el, line_el)
+            end
+            link!(class_el, methods_el)
+            link!(class_el, lines_el)
+            link!(classes_el, class_el)
+        end
+        link!(package_el, classes_el)
+        link!(packages_el, package_el)
+    end
+    link!(root, packages_el)
+
+    return document
+
+end
+
+"""
+$(SIGNATURES)
+
+Get the percentage of lines covered in the total, with formatting
+"""
+_percent(lines_total::Integer, lines_covered::Integer) = lines_total == 0 ? "0.0" : string(lines_covered / lines_total)
+
+
+
 
 end # module
