e821c81d2a115044bf029bf2af8b0d8035b8d099
Tim Wilson <twilsonco@gmail.com>
2023-09-22 10:48:03 -0600

    test: add subtest code to DummyPackage

--- test/DummyPackage/src/DummyPackage.jl
+++ test/DummyPackage/src/DummyPackage.jl
@@ -1,10 +1,13 @@
 "A package for testing coverage generation."
 module DummyPackage
 
-export foo, bar
+export foo, bar, baz, qux, quux, corge, grault
 
 foo() = 42
 
 include("bar.jl")
+include("qux.jl")
+include("corge/grault.jl")
+include("corge/corge.jl")
 
 end
---
+++ test/DummyPackage/src/qux.jl
@@ -0,0 +1,5 @@
+# tested in testset 1
+qux() = 43
+
+# untested
+quux() = 44
